37a666c3526d04a9751a56eff4ca0b80ed2a7216
Javier Pacheco <jepach@gmail.com>
2022-05-05 07:43:55 -0500

    Add reset style for HTML tags

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
+import reset from './styles/reset';
+import { Global } from '@emotion/react';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={reset}/>
     <div>Hello, World!</div>
   </React.StrictMode>
 );
