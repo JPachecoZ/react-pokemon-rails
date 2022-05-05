62477eb6d8b31030d005a9c2a3384ba9a560f88d
Javier Pacheco <jepach@gmail.com>
2022-05-05 07:41:53 -0500

    Initial setup of React + Rails

---
+++ config/importmap.rb
@@ -0,0 +1,14 @@
+# Pin npm packages by running ./bin/importmap
+
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/components', under: 'components'
+pin 'react', to: 'https://ga.jspm.io/npm:react@18.1.0/index.js'
+pin 'react-dom', to: 'https://ga.jspm.io/npm:react-dom@18.1.0/index.js'
+pin 'process', to: 'https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process-production.js'
+pin 'scheduler', to: 'https://ga.jspm.io/npm:scheduler@0.22.0/index.js'
+pin 'object-assign', to: 'https://ga.jspm.io/npm:object-assign@4.1.1/index.js'
+pin 'htm', to: 'https://ga.jspm.io/npm:htm@3.1.1/dist/htm.module.js'
